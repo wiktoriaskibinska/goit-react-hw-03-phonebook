ae9f799112382e24cf9cde86b733706e6085f663
Wiktoria Skibińska <wikas2000@icloud.com>
2024-02-26 12:30:52 +0100

    added local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,7 +17,21 @@ export class App extends Component {
       filter: '',
     };
   }
-  /* this.setState(prevState => ({
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log(this.state.contacts);
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  } /* this.setState(prevState => ({
       contacts: [{ id: nanoid(), ...contact }, ...prevState.contacts],
     }));*/
   addContact = contact => {
